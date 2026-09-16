59ffd36422eb38950ea64255f3e2eea421e82c21
GSZero <52993439+Lxkgit@users.noreply.github.com>
2026-09-16 17:03:37 +0800

    refactor: add application navigation entry

---
+++ app/src/main/java/com/blog/app/navigation/AppNavigation.kt
@@ -0,0 +1,12 @@
+package com.blog.app.navigation
+
+import androidx.compose.runtime.Composable
+import com.blog.app.ui.home.HomeScreen
+
+/**
+ * Application navigation entry point.
+ */
+@Composable
+fun AppNavigation() {
+    HomeScreen()
+}
