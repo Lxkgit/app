589b833fa6eeeb9e24235d49d9112d4cf65f8734
GSZero <52993439+Lxkgit@users.noreply.github.com>
2026-09-17 15:58:37 +0800

    接入 OAuth2 PKCE 登录依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    implementation("net.openid:appauth:0.11.1")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
